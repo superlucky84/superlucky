bf021399174e44f7663fe4be55f51ff3f876cace
root <superlucky84@gmail.com>
2015-01-03 02:54:44 +0900

    j

--- asset/js/controller/editor.js
+++ asset/js/controller/editor.js
@@ -26,11 +26,23 @@ superlucky.service('editorservice',function(){
 
 	// Class 정의  (판 클레스)
 	function editor_pann(){
+		// EDITOR NUM 판
 		this.$DIV = angular.element(document.getElementById('editor_area'));
-		this.$NUM_DIV = angular.element(document.getElementById('editor_num_area'));
-		
 
+		// EDITOR 판
+		this.$NUM_DIV = angular.element(document.getElementById('editor_num_area'));
+
+		// vi모드 (NORMAL, INSERT, COMMAND)
 		this.MODE = "NORMAL";
+
+		// 행
+		this.ROW    = 1;
+
+		// 열
+		this.COLUMN = 1;
+
+		// 전체 행 개수
+		this.TOTAL_ROW = 0;
 	}
 
 	// 파일로드
@@ -42,18 +54,22 @@ superlucky.service('editorservice',function(){
 				var pann = data.data.replace(/[ ]/g,"&nbsp;").split(/\n/);
 				console.log(pann.length);
 
+				var num = 0;
 				for(line_key in pann){
 
 					var line = pann[line_key];
 					if(!line){
 						line = "&nbsp;";
 					}
-					var $SPAN = angular.element("<div>"+line+"</div>");
+					var $SPAN = angular.element("<div class='editor_line'>"+line+"</div>");
 					_this.$DIV.append($SPAN);
-					var num = parseInt(line_key) + 1;
-					_this.$NUM_DIV.append(num+"<br>");
+					num = parseInt(line_key) + 1;
+					_this.$NUM_DIV.append("<div class='editor_line'>"+num+"</div>");
 
 				}
+				_this.TOTAL_ROW = num;
+
+				_this.$DIV.find("div").eq(0).addClass("linehigh");
 
 			}).
 			error(function(data){
@@ -70,6 +86,7 @@ superlucky.service('editorservice',function(){
 			_this.$DIV.attr("contenteditable",true);
 			_this.MODE = "INSERT";
 			document.getElementById("editor_area").focus();
+			$scope.keytarget = "";
 
 		}
 		// 일반모드
@@ -77,6 +94,8 @@ superlucky.service('editorservice',function(){
 			console.log("NORMAL MODE");
 			_this.$DIV.attr("contenteditable",false);
 			_this.MODE = "NORMAL";
+
+
 			document.getElementById("editor_target").focus();
 		}
 		// 명령모드
@@ -92,15 +111,32 @@ superlucky.service('editorservice',function(){
 			if([97,105,111].indexOf(keyCode) > -1){
 				_this.mode_change("INSERT");
 			}
-			if([58].indexOf(keyCode) > -1){
-				_this.mode_change("COMMAND");
+			// 아래로
+			else if(106 == keyCode){
+				if(_this.ROW <= _this.TOTAL_ROW){
+					_this.$DIV.find("div").eq(_this.ROW-1).removeClass("linehigh");
+					_this.$DIV.find("div").eq(_this.ROW).addClass("linehigh");
+					_this.ROW++;
+
+				}
 			}
+			// 위로
+			else if(107 == keyCode){
+				if(_this.ROW > 1){
+					_this.$DIV.find("div").eq(_this.ROW-1).removeClass("linehigh");
+					_this.$DIV.find("div").eq(_this.ROW-2).addClass("linehigh");
+					_this.ROW--;
+				}
+			}
+			console.log(_this.ROW);
+			//else if([58].indexOf(keyCode) > -1){
+				//_this.mode_change("COMMAND");
+			//}
 		}
 	}
 	// 키코드 esc
 	editor_pann.prototype.key_esc = function(keyCode){
 		var _this = this;
-		console.log(_this.MODE);
 		if(_this.MODE!="NORMAL" && keyCode==27){
 			console.log('jj');
 			_this.mode_change("NORMAL");
@@ -117,7 +153,6 @@ superlucky.service('editorservice',function(){
 		return keyCode;
 	}
 
-
 	// Class 정의 (라인 클레스)
 	function editor_line(){
 	}
@@ -132,7 +167,6 @@ superlucky.service('editorservice',function(){
 	$scope.key_press = function(event){
 		var keyCode = editorPann.keycode(event);
 		editorPann.key_fun(keyCode);
-		$scope.keytarget = "";
 	}
 	$scope.key_down = function(event){
 		var keyCode = editorPann.keycode(event);
@@ -147,7 +181,6 @@ superlucky.service('editorservice',function(){
 			document.getElementById("editor_area").focus();
 		}
 	}
-
 	document.getElementById("editor_target").focus();
 
 });
