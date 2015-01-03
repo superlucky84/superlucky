f3500a210c7497f115a145ec207fb21a38b4c91f
root <superlucky84@gmail.com>
2015-01-04 00:58:23 +0900

    vim job

--- asset/js/controller/editor.js
+++ asset/js/controller/editor.js
@@ -3,6 +3,35 @@
 // 컨트롤러 간 공유
 superlucky.service('editorservice',function(){
 
+	this.htmlspecialchars = function(str,except_tag) {
+		if (typeof(str) == "string") {
+			str = str.replace(/&/g, "&amp;"); /* must do &amp; first */
+			str = str.replace(/"/g, "&quot;");
+			str = str.replace(/'/g, "&#039;");
+			if(!except_tag){
+				str = str.replace(/</g, "&lt;");
+				str = str.replace(/>/g, "&gt;");
+			}
+			str = str.replace(/ /g, "&nbsp;");
+		}
+		return str;
+	}
+	this.rhtmlspecialchars = function(str,except_tag) {
+		if (typeof(str) == "string") {
+			str = str.replace(/&nbsp;/ig, " ");
+			if(!except_tag){
+				str = str.replace(/&gt;/ig, ">");
+				str = str.replace(/&lt;/ig, "<");
+			}
+			str = str.replace(/&#039;/g, "'");
+			str = str.replace(/&quot;/ig, '"');
+			str = str.replace(/&amp;/ig, '&'); /* must do &amp; last */
+		}
+		return str;
+	}
+
+
+
 	this.dialog_instance = null;
 	this.main_scope = null;
 	this.main_scope_setting = function($scope){
@@ -53,7 +82,6 @@ superlucky.service('editorservice',function(){
 
 						var pann = data.data.replace(/[\&]/g,"&amp;").replace(/[ ]/g,"&nbsp;").split(/\n/);
 						//var pann = data.data.replace(/[ ]/g,"&nbsp;").split(/\n/);
-						console.log(pann.length);
 
 						var num = 0;
 						for(line_key in pann){
@@ -73,8 +101,6 @@ superlucky.service('editorservice',function(){
 						_this.line_ins = new editor_line();
 
 						//_this.$DIV.find("div").eq(0).addClass("linehigh");
-
-
 						_this.$DIV.attr("contenteditable",true);
 
 					}).
@@ -93,7 +119,15 @@ superlucky.service('editorservice',function(){
 					console.log("INSERT MODE");
 					//_this.$DIV.attr("contenteditable",true);
 					_this.MODE = "INSERT";
+
+
+					//_this.$DIV.find(".editor_cursor").css("width","1px");
+
 					document.getElementById("editor_area").focus();
+					_this.$DIV.find(".editor_cursor").hide();
+
+					// 입력 모드에 커서위치 잡기
+					_this.line_ins.set_insert_cursor(_this.$DIV.find("div").eq(_this.ROW+1),_this.COLUMN);
 
 				}
 				// 일반모드
@@ -101,8 +135,25 @@ superlucky.service('editorservice',function(){
 					console.log("NORMAL MODE");
 					//_this.$DIV.attr("contenteditable",false);
 					_this.MODE = "NORMAL";
+					$scope.keytarget = "";
+
+					// 커서위치 재정렬
+					_this.COLUMN--;
+
+					//_this.$DIV.find(".editor_cursor").css("width","10px");
 
 					document.getElementById("editor_target").focus();
+					_this.$DIV.find(".editor_cursor").show();
+
+					// 일반모드에 커서위치 잡기
+					var diff_len = _this.line_ins.set_normal_cursor(_this.$DIV.find("div"),_this.ROW);
+					_this.COLUMN = _this.COLUMN + diff_len;
+
+					// 컬럼리셋
+					_this.line_ins.analisys(_this.$DIV.find("div").eq(_this.ROW+1));
+					_this.cursor_column_reset();
+
+
 				}
 				// 명령모드
 				else if(TYPE=="COMMAND"){
@@ -112,15 +163,14 @@ superlucky.service('editorservice',function(){
 		}
 		// 컬럼위치 재정렬
 		if(typeof this.cursor_column_reset != "function"){
-			editor_pann.prototype.cursor_column_reset = function(){
+			editor_pann.prototype.cursor_column_reset = function(reset_type){
 				var _this = this;
-				if(_this.line_ins.end < _this.COLUMN){
+				if(_this.line_ins.end < _this.COLUMN && !reset_type){
 					var left = (_this.line_ins.end-1) * _this.font_width;
-					_this.$DIV.find(".editor_cursor").css("left",left+"px");
 				}else{
 					var left = (_this.COLUMN-1) * _this.font_width;
-					_this.$DIV.find(".editor_cursor").css("left",left+"px");
 				}
+				_this.$DIV.find(".editor_cursor").css("left",left+"px");
 			}
 		}
 		// 키코드 액션
@@ -129,10 +179,39 @@ superlucky.service('editorservice',function(){
 				var _this = this;
 				console.log(keyCode);
 
-				$scope.keytarget = "";
 
-				if(_this.MODE=="NORMAL"){
-					if([97,105,111].indexOf(keyCode) > -1){
+				// 입력모드일때 바인딩
+				if(_this.MODE=="INSERT"){
+
+					/*
+					var orig_st = _this.$DIV.find("div").eq(_this.ROW+1).html().replace(/&nbsp;/g," ");
+					var res = String.fromCharCode(keyCode);
+					var pre_string  = orig_st.substr(0,_this.COLUMN-1);
+					var next_string = orig_st.substring(_this.COLUMN-1,orig_st.length);
+					var return_text = pre_string+res+next_string;
+					return_text = return_text.replace(/ /g,"&nbsp;");
+					_this.$DIV.find("div").eq(_this.ROW+1).html(return_text);
+					_this.COLUMN++;
+					// 커서위치 재정렬
+					_this.cursor_column_reset();
+					*/
+
+				}
+				// 일반모드일때 바인딩
+				else if(_this.MODE=="NORMAL"){
+					if([65,97,73,105,111].indexOf(keyCode) > -1){
+						if(97 == keyCode){
+							_this.COLUMN++;
+							_this.cursor_column_reset("FORCE");
+						}
+						else if(65 == keyCode){
+							_this.COLUMN = _this.line_ins.end+1;
+							_this.cursor_column_reset("FORCE");
+						}
+						else if(73 == keyCode){
+							_this.COLUMN = _this.line_ins.start+1;
+							_this.cursor_column_reset();
+						}
 						_this.mode_change("INSERT");
 					}
 					// 왼쪽
@@ -241,8 +320,12 @@ superlucky.service('editorservice',function(){
 					// 되돌리기
 					else if(117 == keyCode){
 						//_this.$DIV.attr("contenteditable",true);
+
+						//document.getElementById("editor_area").focus();
 						document.execCommand('undo', false, null);
 						document.execCommand('undo', false, null);
+						document.execCommand('undo', false, null);
+						//document.getElementById("editor_target").focus();
 						//document.getElementById("editor_area").execCommand('undo', false, null);
 						//_this.$DIV.attr("contenteditable",false);
 					}
@@ -257,7 +340,6 @@ superlucky.service('editorservice',function(){
 			editor_pann.prototype.key_esc = function(keyCode){
 				var _this = this;
 				if(_this.MODE!="NORMAL" && keyCode==27){
-					console.log('jj');
 					_this.mode_change("NORMAL");
 				}
 			}
@@ -359,6 +441,44 @@ superlucky.service('editorservice',function(){
 				return cursor;
 			}
 		}
+
+		// 일반모드에 커서위치 잡기
+		if(typeof this.set_normal_cursor != "function"){
+			editor_line.prototype.set_normal_cursor = function(lines,row){
+				var _this = this;
+				var org_length = _this.line_string.length;
+				var new_length = lines.eq(row+1).html().replace(/(&nbsp;|&amp;)/g," ").length;
+
+				return new_length - org_length;
+			}
+		}
+
+		// 입력 모드에 커서위치 잡기
+		if(typeof this.set_insert_cursor != "function"){
+			editor_line.prototype.set_insert_cursor = function(line,column){
+
+				//var orig_st = line.html().replace(/&nbsp;/g," ");
+				var orig_st = editorservice.rhtmlspecialchars(line.html(),true);
+				var pre_string  = orig_st.substr(0,column-1);
+				var next_string = orig_st.substring(column-1,orig_st.length);
+
+				var return_text = pre_string+"<caret>caret</caret>"+next_string;
+				//return_text = return_text.replace(/ /g,"&nbsp;");
+				return_text = editorservice.htmlspecialchars(return_text,true);
+
+				//console.log(return_text);
+				line.html(return_text);
+				var cc = line.find("caret")[0];
+				var range = document.createRange();
+				range.selectNode(cc);
+				var selection = window.getSelection();
+				selection.removeAllRanges();
+				selection.addRange(range);
+				range.deleteContents();
+
+			}
+		}
+
 	}
 	// Class 정의 (블럭클레스)
 	function editor_block(){
@@ -368,9 +488,10 @@ superlucky.service('editorservice',function(){
 	var editorPann = new editor_pann();
 	editorPann.file_load();
 
-
 	$scope.key_press = function(event){
 		var keyCode = editorPann.keycode(event);
+
+		
 		editorPann.key_fun(keyCode);
 	}
 	$scope.key_down = function(event){
